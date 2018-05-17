f6aa97c01a688888e884de02f3e6ce79497d9d33
one-for-all <jianan.li@yahoo.com>
2018-05-16 22:26:51 -0700

    finished notebook analysis

---
+++ code/perception.py
@@ -0,0 +1,169 @@
+import numpy as np
+import cv2
+
+
+# Identify pixels above the threshold
+# Threshold of RGB > 160 does a nice job of identifying ground pixels only
+def color_thresh(img, rgb_thresh=(160, 160, 160)):
+    # rock thresholds
+    rock_threshold = (130, 110, 0)
+    rock_upper = (220, 180, 60)
+
+    # Create an array of zeros same xy size as img, but single channel
+    color_select = np.zeros_like(img[:, :, 0])
+    obstacles = np.zeros_like(img[:, :, 0])
+    rocks = np.zeros_like(img[:, :, 0])
+    # Require that each pixel be above all three threshold values in RGB
+    # above_thresh will now contain a boolean array with "True"
+    # where threshold was met
+    above_thresh = (img[:, :, 0] > rgb_thresh[0]) \
+                   & (img[:, :, 1] > rgb_thresh[1]) \
+                   & (img[:, :, 2] > rgb_thresh[2])
+
+    obstacles_thresh = ~above_thresh
+
+    rock_above_threshold = (img[:, :, 0] > rock_threshold[0]) & (
+    img[:, :, 0] < rock_upper[0]) \
+                           & (img[:, :, 1] > rock_threshold[1]) & (
+                           img[:, :, 1] < rock_upper[1]) \
+                           & (img[:, :, 2] > rock_threshold[2]) & (
+                           img[:, :, 2] < rock_upper[2])
+    # Index the array of zeros with the boolean array and set to 1
+    color_select[above_thresh] = 1
+    color_select[rock_above_threshold] = 1
+    obstacles[obstacles_thresh] = 1
+    obstacles[rock_above_threshold] = 0
+    rocks[rock_above_threshold] = 1
+    # Return the binary image
+    return color_select, obstacles, rocks
+
+# Define a function to convert from image coords to rover coords
+def rover_coords(binary_img):
+    # Identify nonzero pixels
+    ypos, xpos = binary_img.nonzero()
+    # Calculate pixel positions with reference to the rover position being at the 
+    # center bottom of the image.  
+    x_pixel = -(ypos - binary_img.shape[0]).astype(np.float)
+    y_pixel = -(xpos - binary_img.shape[1]/2 ).astype(np.float)
+    return x_pixel, y_pixel
+
+
+# Define a function to convert to radial coords in rover space
+def to_polar_coords(x_pixel, y_pixel):
+    # Convert (x_pixel, y_pixel) to (distance, angle) 
+    # in polar coordinates in rover space
+    # Calculate distance to each pixel
+    dist = np.sqrt(x_pixel**2 + y_pixel**2)
+    # Calculate angle away from vertical for each pixel
+    angles = np.arctan2(y_pixel, x_pixel)
+    return dist, angles
+
+# Define a function to map rover space pixels to world space
+def rotate_pix(xpix, ypix, yaw):
+    # Convert yaw to radians
+    yaw_rad = yaw * np.pi / 180
+    xpix_rotated = (xpix * np.cos(yaw_rad)) - (ypix * np.sin(yaw_rad))
+                            
+    ypix_rotated = (xpix * np.sin(yaw_rad)) + (ypix * np.cos(yaw_rad))
+    # Return the result  
+    return xpix_rotated, ypix_rotated
+
+def translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale): 
+    # Apply a scaling and a translation
+    xpix_translated = (xpix_rot / scale) + xpos
+    ypix_translated = (ypix_rot / scale) + ypos
+    # Return the result  
+    return xpix_translated, ypix_translated
+
+
+# Define a function to apply rotation and translation (and clipping)
+# Once you define the two functions above this function should work
+def pix_to_world(xpix, ypix, xpos, ypos, yaw, world_size, scale):
+    # Apply rotation
+    xpix_rot, ypix_rot = rotate_pix(xpix, ypix, yaw)
+    # Apply translation
+    xpix_tran, ypix_tran = translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale)
+    # Perform rotation, translation and clipping all at once
+    x_pix_world = np.clip(np.int_(xpix_tran), 0, world_size - 1)
+    y_pix_world = np.clip(np.int_(ypix_tran), 0, world_size - 1)
+    # Return the result
+    return x_pix_world, y_pix_world
+
+# Define a function to perform a perspective transform
+def perspect_transform(img, src, dst):
+           
+    M = cv2.getPerspectiveTransform(src, dst)
+    warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))# keep same size as input image
+    
+    return warped
+
+
+# Apply the above functions in succession and update the Rover state accordingly
+def perception_step(Rover):
+    # Perform perception steps to update Rover()
+    # TODO: 
+    # NOTE: camera image is coming to you in Rover.img
+    # 1) Define source and destination points for perspective transform
+    dst_size = 5
+    bottom_offset = 6
+    source = np.float32([[14, 140], [301, 140], [200, 96], [118, 96]])
+    destination = np.float32(
+        [[Rover.img.shape[1] / 2 - dst_size, Rover.img.shape[0] - bottom_offset],
+         [Rover.img.shape[1] / 2 + dst_size, Rover.img.shape[0] - bottom_offset],
+         [Rover.img.shape[1] / 2 + dst_size,
+          Rover.img.shape[0] - 2 * dst_size - bottom_offset],
+         [Rover.img.shape[1] / 2 - dst_size,
+          Rover.img.shape[0] - 2 * dst_size - bottom_offset],
+         ])
+    # 2) Apply perspective transform
+    warped = perspect_transform(Rover.img, source, destination)
+    # 3) Apply color threshold to identify navigable terrain/obstacles/rock samples
+    threshed, obstacles, rocks = color_thresh(warped)
+    # 4) Update Rover.vision_image (this will be displayed on left side of screen)
+        # Example: Rover.vision_image[:,:,0] = obstacle color-thresholded binary image
+        #          Rover.vision_image[:,:,1] = rock_sample color-thresholded binary image
+        #          Rover.vision_image[:,:,2] = navigable terrain color-thresholded binary image
+    Rover.vision_image[:, :, 0] = obstacles
+    Rover.vision_image[:, :, 1] = rocks
+    Rover.vision_image[:, :, 2] = threshed
+    # 5) Convert map image pixel values to rover-centric coords
+    xpix, ypix = rover_coords(threshed)
+    xpix_obstacles, ypix_obstacles = rover_coords(obstacles)
+    xpix_rocks, ypix_rocks = rover_coords(rocks)
+    # 6) Convert rover-centric pixel values to world coordinates
+    navigable_x_world, navigable_y_world = pix_to_world(xpix, ypix,
+                                                        Rover.pos[0],
+                                                        Rover.pos[1],
+                                                        Rover.yaw,
+                                                        Rover.worldmap.shape[0],
+                                                        10)
+    obstacle_x_world, obstacle_y_world = pix_to_world(xpix_obstacles,
+                                                      ypix_obstacles,
+                                                      Rover.pos[0],
+                                                      Rover.pos[1],
+                                                      Rover.yaw,
+                                                      Rover.worldmap.shape[0],
+                                                      10)
+    rock_x_world, rock_y_world = pix_to_world(xpix_rocks, ypix_rocks,
+                                              Rover.pos[0],
+                                              Rover.pos[1],
+                                              Rover.yaw,
+                                              Rover.worldmap.shape[0], 10)
+    # 7) Update Rover worldmap (to be displayed on right side of screen)
+        # Example: Rover.worldmap[obstacle_y_world, obstacle_x_world, 0] += 1
+        #          Rover.worldmap[rock_y_world, rock_x_world, 1] += 1
+        #          Rover.worldmap[navigable_y_world, navigable_x_world, 2] += 1
+    if Rover.roll < 1 and Rover.pitch < 1:
+        Rover.worldmap[navigable_y_world, navigable_x_world, 2] += 1
+        Rover.worldmap[obstacle_y_world, obstacle_x_world, 0] += 1
+        Rover.worldmap[rock_y_world, rock_x_world, 1] += 1
+
+    # 8) Convert rover-centric pixel positions to polar coordinates
+    # Update Rover pixel distances and angles
+        # Rover.nav_dists = rover_centric_pixel_distances
+        # Rover.nav_angles = rover_centric_angles
+    dist, angles = to_polar_coords(xpix, ypix)
+    Rover.nav_dists = dist
+    Rover.nav_angles = angles
+    
+    return Rover
---
+++ writeup.md
@@ -0,0 +1,73 @@
+## Project: Search and Sample Return
+---
+
+
+**The goals / steps of this project are the following:**  
+
+**Training / Calibration**  
+
+* Download the simulator and take data in "Training Mode"
+* Test out the functions in the Jupyter Notebook provided
+* Add functions to detect obstacles and samples of interest (golden rocks)
+* Fill in the `process_image()` function with the appropriate image processing steps (perspective transform, color threshold etc.) to get from raw images to a map.  The `output_image` you create in this step should demonstrate that your mapping pipeline works.
+* Use `moviepy` to process the images in your saved dataset with the `process_image()` function.  Include the video you produce as part of your submission.
+
+**Autonomous Navigation / Mapping**
+
+* Fill in the `perception_step()` function within the `perception.py` script with the appropriate image processing functions to create a map and update `Rover()` data (similar to what you did with `process_image()` in the notebook). 
+* Fill in the `decision_step()` function within the `decision.py` script with conditional statements that take into consideration the outputs of the `perception_step()` in deciding how to issue throttle, brake and steering commands. 
+* Iterate on your perception and decision function until your rover does a reasonable (need to define metric) job of navigating and mapping.  
+
+[//]: # (Image References)
+
+[rock_image]: ./calibration_images/example_rock1.jpg
+[color_classificiation]: ./misc/color_classification.png
+
+## [Rubric](https://review.udacity.com/#!/rubrics/916/view) Points
+### Here I will consider the rubric points individually and describe how I addressed each point in my implementation.  
+
+---
+### Writeup / README
+
+#### 1. Provide a Writeup / README that includes all the rubric points and how you addressed each one.  You can submit your writeup as markdown or pdf.
+
+You're reading it!
+
+### Notebook Analysis
+#### 1. Run the functions provided in the notebook on test images (first with the test data provided, next on data you have recorded). Add/modify functions to allow for color selection of obstacles and rock samples.
+
+In [Rover_Project_Test_Notebook.ipynb] (./code/master/Rover_Project_Test_Notebook.ipynb), the implementation for rock and obstacle color selection in written in function **color_thresh**.    
+The range of rgb values for rock is hardcoded, (by inspection of color values of rocks in sample images). The rocks are where rgb values fall in this range.    
+The obstacles are where neither covered by ground nor rocks.
+
+Original image:
+![original rock image][rock_image]
+
+Identification:
+![object identification by color][color_classificiation]
+
+#### 1. Populate the `process_image()` function with the appropriate analysis steps to map pixels identifying navigable terrain, obstacles and rock samples into a worldmap.  Run `process_image()` on your test data using the `moviepy` functions provided to create video output of your result. 
+The camera image is processed in the following way:
+1. The image is mapped from camera perspective to top-down perspective.
+2. Ground, obstacles, and rocks are identified using color thresholding.
+3. The coordinates are transformed from image coordinates to rover coordinates and then to world coordinates.
+4. For worldmap, blue channel value is increased for each identification of ground, red for obstacle, and green for rock.
+5. A mosaic image is created, consisting of original camera image, top-down view, worldmap overlayed with ground truth, and text indicating when rocks have been identified.
+
+A example video output can be found at [./output/test_mapping.mp4] (./output/test_mapping.mp4).
+### Autonomous Navigation and Mapping
+
+#### 1. Fill in the `perception_step()` (at the bottom of the `perception.py` script) and `decision_step()` (in `decision.py`) functions in the autonomous mapping scripts and an explanation is provided in the writeup of how and why these functions were modified as they were.
+
+
+#### 2. Launching in autonomous mode your rover can navigate and map autonomously.  Explain your results and how you might improve them in your writeup.  
+
+**Note: running the simulator with different choices of resolution and graphics quality may produce different results, particularly on different machines!  Make a note of your simulator settings (resolution and graphics quality set on launch) and frames per second (FPS output to terminal by `drive_rover.py`) in your writeup when you submit the project so your reviewer can reproduce your results.**
+
+Here I'll talk about the approach I took, what techniques I used, what worked and why, where the pipeline might fail and how I might improve it if I were going to pursue this project further.  
+
+
+
+![alt text][image3]
+
+
